6c6492df9d177e5f6f60e13726c403884c5996cb
Martin Beck <martin.beck2@gmx.de>
2026-09-15 06:17:41 +0200

    test: reject missing durable barriers before lock acquisition
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tests/test_lock_domain_scope.py
+++ tests/test_lock_domain_scope.py
@@ -654,6 +654,16 @@ class LockDomainScopeTests(unittest.TestCase):
         self.assertEqual([], common_calls)
         self.assertFalse(self.session.operation_owned_by_current_thread)
 
+        missing_barrier_context = dict(stale_context)
+        missing_barrier_context.pop("durable_barrier_id")
+        with (
+            self.assertRaisesRegex(LockDomainError, "does not match"),
+            scope.validated_hold(missing_barrier_context),
+        ):
+            self.fail("missing durable barrier identifier must fail before scope acquisition")
+        self.assertEqual([], common_calls)
+        self.assertFalse(self.session.operation_owned_by_current_thread)
+
         missing_owner_context = dict(stale_context)
         missing_owner_context.pop("fencing_owner")
         with (
