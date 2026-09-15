787479904f179982d3ba4f887e0edebfeacf5b0e
Martin Beck <martin.beck2@gmx.de>
2026-09-15 07:53:56 +0200

    test: reject tuple durable barriers before lock acquisition
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tests/test_lock_domain_scope.py
+++ tests/test_lock_domain_scope.py
@@ -1275,6 +1275,16 @@ class LockDomainScopeTests(unittest.TestCase):
         self.assertEqual([], common_calls)
         self.assertFalse(self.session.operation_owned_by_current_thread)
 
+        tuple_barrier_context = dict(stale_context)
+        tuple_barrier_context["durable_barrier_id"] = ("barrier-1",)
+        with (
+            self.assertRaisesRegex(LockDomainError, "does not match"),
+            scope.validated_hold(tuple_barrier_context),
+        ):
+            self.fail("tuple durable barrier identifier must fail before scope acquisition")
+        self.assertEqual([], common_calls)
+        self.assertFalse(self.session.operation_owned_by_current_thread)
+
         non_text_barrier_context = dict(stale_context)
         non_text_barrier_context["durable_barrier_id"] = 13
         with (
