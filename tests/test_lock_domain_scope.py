70068863fb3e0956ea958f9507ef49fd22746ea3
martin-beck <martin.beck2@gmx.de>
2026-09-15 08:51:24 +0200

    test: reject memoryview fencing tokens (#240)
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tests/test_lock_domain_scope.py
+++ tests/test_lock_domain_scope.py
@@ -1305,6 +1305,16 @@ class LockDomainScopeTests(unittest.TestCase):
         self.assertEqual([], common_calls)
         self.assertFalse(self.session.operation_owned_by_current_thread)
 
+        memoryview_fence_context = dict(stale_context)
+        memoryview_fence_context["fencing_token"] = memoryview(b"fence-1")
+        with (
+            self.assertRaisesRegex(LockDomainError, "does not match"),
+            scope.validated_hold(memoryview_fence_context),
+        ):
+            self.fail("memoryview fencing token must fail before scope acquisition")
+        self.assertEqual([], common_calls)
+        self.assertFalse(self.session.operation_owned_by_current_thread)
+
         bool_barrier_context = dict(stale_context)
         bool_barrier_context["durable_barrier_id"] = True
         with (
