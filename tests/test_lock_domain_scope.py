7f50da598b41d43d90258a60eed0a1516b452502
Martin Beck <martin.beck2@gmx.de>
2026-09-15 07:25:27 +0200

    test: reject set project identifiers before lock acquisition
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tests/test_lock_domain_scope.py
+++ tests/test_lock_domain_scope.py
@@ -1155,6 +1155,16 @@ class LockDomainScopeTests(unittest.TestCase):
         self.assertEqual([], common_calls)
         self.assertFalse(self.session.operation_owned_by_current_thread)
 
+        set_project_context = dict(stale_context)
+        set_project_context["project_id"] = {PROJECT}
+        with (
+            self.assertRaisesRegex(LockDomainError, "does not match"),
+            scope.validated_hold(set_project_context),
+        ):
+            self.fail("set project identifier must fail before scope acquisition")
+        self.assertEqual([], common_calls)
+        self.assertFalse(self.session.operation_owned_by_current_thread)
+
         non_text_fence_context = dict(stale_context)
         non_text_fence_context["fencing_token"] = 7
         with (
