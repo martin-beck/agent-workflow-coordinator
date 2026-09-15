3aefd522b3747bdc40a9a592e1b5f2d295a1230b
Martin Beck <martin.beck2@gmx.de>
2026-09-15 08:23:15 +0200

    test: reject mapping durable barriers before lock acquisition
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tests/test_lock_domain_scope.py
+++ tests/test_lock_domain_scope.py
@@ -1365,6 +1365,16 @@ class LockDomainScopeTests(unittest.TestCase):
         self.assertEqual([], common_calls)
         self.assertFalse(self.session.operation_owned_by_current_thread)
 
+        map_barrier_context = dict(stale_context)
+        map_barrier_context["durable_barrier_id"] = {"barrier": "barrier-1"}
+        with (
+            self.assertRaisesRegex(LockDomainError, "does not match"),
+            scope.validated_hold(map_barrier_context),
+        ):
+            self.fail("mapping durable barrier identifier must fail before scope acquisition")
+        self.assertEqual([], common_calls)
+        self.assertFalse(self.session.operation_owned_by_current_thread)
+
         non_text_barrier_context = dict(stale_context)
         non_text_barrier_context["durable_barrier_id"] = 13
         with (
