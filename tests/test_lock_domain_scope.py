c6bfeb5c2311b4f11cc24677b233cfcb23db4eaf
Martin Beck <martin.beck2@gmx.de>
2026-09-15 06:06:26 +0200

    test: reject missing authority revisions before lock acquisition
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tests/test_lock_domain_scope.py
+++ tests/test_lock_domain_scope.py
@@ -654,6 +654,16 @@ class LockDomainScopeTests(unittest.TestCase):
         self.assertEqual([], common_calls)
         self.assertFalse(self.session.operation_owned_by_current_thread)
 
+        missing_authority_context = dict(stale_context)
+        missing_authority_context.pop("authority_revision")
+        with (
+            self.assertRaisesRegex(LockDomainError, "does not match"),
+            scope.validated_hold(missing_authority_context),
+        ):
+            self.fail("missing authority revision must fail before scope acquisition")
+        self.assertEqual([], common_calls)
+        self.assertFalse(self.session.operation_owned_by_current_thread)
+
         replaced_authority_context = dict(stale_context)
         replaced_authority_context["authority_revision"] = "authority-2"
         with (
