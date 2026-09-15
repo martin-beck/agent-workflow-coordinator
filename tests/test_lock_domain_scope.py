c4deb8216d54b1887defbe739fbe28bec2b7139d
Martin Beck <martin.beck2@gmx.de>
2026-09-15 04:31:30 +0200

    test: reject float state revisions before lock acquisition
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tests/test_lock_domain_scope.py
+++ tests/test_lock_domain_scope.py
@@ -654,6 +654,16 @@ class LockDomainScopeTests(unittest.TestCase):
         self.assertEqual([], common_calls)
         self.assertFalse(self.session.operation_owned_by_current_thread)
 
+        float_revision_context = dict(stale_context)
+        float_revision_context["state_revision"] = 1.0
+        with (
+            self.assertRaisesRegex(LockDomainError, "does not match"),
+            scope.validated_hold(float_revision_context),
+        ):
+            self.fail("float state revision must fail before scope acquisition")
+        self.assertEqual([], common_calls)
+        self.assertFalse(self.session.operation_owned_by_current_thread)
+
         negative_revision_context = dict(stale_context)
         negative_revision_context["state_revision"] = -1
         with (
