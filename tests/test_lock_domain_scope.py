1c72db48d1c1a4fd777c573a43fcb8da2f85a60f
Martin Beck <martin.beck2@gmx.de>
2026-09-15 04:45:14 +0200

    test: reject sequence state revisions before lock acquisition
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tests/test_lock_domain_scope.py
+++ tests/test_lock_domain_scope.py
@@ -654,6 +654,16 @@ class LockDomainScopeTests(unittest.TestCase):
         self.assertEqual([], common_calls)
         self.assertFalse(self.session.operation_owned_by_current_thread)
 
+        sequence_revision_context = dict(stale_context)
+        sequence_revision_context["state_revision"] = [1]
+        with (
+            self.assertRaisesRegex(LockDomainError, "does not match"),
+            scope.validated_hold(sequence_revision_context),
+        ):
+            self.fail("sequence state revision must fail before scope acquisition")
+        self.assertEqual([], common_calls)
+        self.assertFalse(self.session.operation_owned_by_current_thread)
+
         null_revision_context = dict(stale_context)
         null_revision_context["state_revision"] = None
         with (
