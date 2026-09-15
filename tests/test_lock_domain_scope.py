565a4362fbe4902168e003222ec78fa8e1fb7112
Martin Beck <martin.beck2@gmx.de>
2026-09-15 07:38:19 +0200

    test: reject list fencing owners before lock acquisition
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tests/test_lock_domain_scope.py
+++ tests/test_lock_domain_scope.py
@@ -1044,6 +1044,16 @@ class LockDomainScopeTests(unittest.TestCase):
         self.assertEqual([], common_calls)
         self.assertFalse(self.session.operation_owned_by_current_thread)
 
+        list_owner_context = dict(stale_context)
+        list_owner_context["fencing_owner"] = ["owner-1"]
+        with (
+            self.assertRaisesRegex(LockDomainError, "does not match"),
+            scope.validated_hold(list_owner_context),
+        ):
+            self.fail("list fencing owner must fail before scope acquisition")
+        self.assertEqual([], common_calls)
+        self.assertFalse(self.session.operation_owned_by_current_thread)
+
         missing_context = dict(stale_context)
         missing_context.pop("fencing_owner")
         with (
