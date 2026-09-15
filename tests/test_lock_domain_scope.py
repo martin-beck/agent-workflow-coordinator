cf4836e8d6e54ff06d06385f61583cb530f2cd13
Martin Beck <martin.beck2@gmx.de>
2026-09-15 08:34:29 +0200

    test: reject bytearray fencing owners before lock acquisition
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tests/test_lock_domain_scope.py
+++ tests/test_lock_domain_scope.py
@@ -1084,6 +1084,16 @@ class LockDomainScopeTests(unittest.TestCase):
         self.assertEqual([], common_calls)
         self.assertFalse(self.session.operation_owned_by_current_thread)
 
+        bytearray_owner_context = dict(stale_context)
+        bytearray_owner_context["fencing_owner"] = bytearray(b"owner-1")
+        with (
+            self.assertRaisesRegex(LockDomainError, "does not match"),
+            scope.validated_hold(bytearray_owner_context),
+        ):
+            self.fail("bytearray fencing owner must fail before scope acquisition")
+        self.assertEqual([], common_calls)
+        self.assertFalse(self.session.operation_owned_by_current_thread)
+
         missing_context = dict(stale_context)
         missing_context.pop("fencing_owner")
         with (
