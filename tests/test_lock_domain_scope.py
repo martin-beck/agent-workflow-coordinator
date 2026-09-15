8a4701a8a201989f57edc4be1547d2ccace00506
Martin Beck <martin.beck2@gmx.de>
2026-09-15 07:35:10 +0200

    test: reject tuple authority revisions before lock acquisition
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tests/test_lock_domain_scope.py
+++ tests/test_lock_domain_scope.py
@@ -1095,6 +1095,16 @@ class LockDomainScopeTests(unittest.TestCase):
         self.assertEqual([], common_calls)
         self.assertFalse(self.session.operation_owned_by_current_thread)
 
+        tuple_authority_context = dict(stale_context)
+        tuple_authority_context["authority_revision"] = ("authority-stale",)
+        with (
+            self.assertRaisesRegex(LockDomainError, "does not match"),
+            scope.validated_hold(tuple_authority_context),
+        ):
+            self.fail("tuple authority revision must fail before scope acquisition")
+        self.assertEqual([], common_calls)
+        self.assertFalse(self.session.operation_owned_by_current_thread)
+
         non_text_project_context = dict(stale_context)
         non_text_project_context["project_id"] = 111
         with (
