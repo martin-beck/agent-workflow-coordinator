88103fc31c9b3503adc21f211e689a003b15de81
Martin Beck <martin.beck2@gmx.de>
2026-09-15 07:31:58 +0200

    test: reject list authority revisions before lock acquisition
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tests/test_lock_domain_scope.py
+++ tests/test_lock_domain_scope.py
@@ -1085,6 +1085,16 @@ class LockDomainScopeTests(unittest.TestCase):
         self.assertEqual([], common_calls)
         self.assertFalse(self.session.operation_owned_by_current_thread)
 
+        list_authority_context = dict(stale_context)
+        list_authority_context["authority_revision"] = ["authority-stale"]
+        with (
+            self.assertRaisesRegex(LockDomainError, "does not match"),
+            scope.validated_hold(list_authority_context),
+        ):
+            self.fail("list authority revision must fail before scope acquisition")
+        self.assertEqual([], common_calls)
+        self.assertFalse(self.session.operation_owned_by_current_thread)
+
         non_text_project_context = dict(stale_context)
         non_text_project_context["project_id"] = 111
         with (
