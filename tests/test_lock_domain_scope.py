2deb3eacdab339650d14aa75ef362d342be10456
Martin Beck <martin.beck2@gmx.de>
2026-09-15 05:47:04 +0200

    test: reject memoryview state revisions before lock acquisition
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tests/test_lock_domain_scope.py
+++ tests/test_lock_domain_scope.py
@@ -654,6 +654,16 @@ class LockDomainScopeTests(unittest.TestCase):
         self.assertEqual([], common_calls)
         self.assertFalse(self.session.operation_owned_by_current_thread)
 
+        memoryview_revision_context = dict(stale_context)
+        memoryview_revision_context["state_revision"] = memoryview(b"1")
+        with (
+            self.assertRaisesRegex(LockDomainError, "does not match"),
+            scope.validated_hold(memoryview_revision_context),
+        ):
+            self.fail("memoryview state revision must fail before scope acquisition")
+        self.assertEqual([], common_calls)
+        self.assertFalse(self.session.operation_owned_by_current_thread)
+
         stale_revision_context = dict(stale_context)
         stale_revision_context["state_revision"] = 2
         with (
