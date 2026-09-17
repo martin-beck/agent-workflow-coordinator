523d5cbb637ff3cc4e7028508afa7e365ac74ca7
Martin Beck <martin.beck@gmx.de>
2026-09-17 22:07:43 +0200

    test(release): cover privacy-safe signer output
    Signed-off-by: Martin Beck <martin.beck@gmx.de>

--- tests/test_release_workflow.py
+++ tests/test_release_workflow.py
@@ -28,7 +28,7 @@ def test_release_signer_template_is_self_test_only() -> None:
     assert "@VENDOR_MANIFEST@" in template
     assert "@OUTPUT@" in template
     assert 'rev-parse HEAD' in template
-    assert '"@RELEASE_VERSION@"' in template
+    assert "@RELEASE_VERSION@" in template
     assert '"@SOURCE_COMMIT@"' in template
     assert "sign" in template.lower()
     assert "git push" not in template
