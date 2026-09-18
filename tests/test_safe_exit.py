da9da77204f6de89866f09f77b1517bd280fbf1a
martin-beck <martin.beck2@gmx.de>
2026-09-18 12:35:52 +0200

    test: type hostile coverage fixtures (#687)
    Signed-off-by: martin-beck <martin.beck2@gmx.de>

--- tests/test_safe_exit.py
+++ tests/test_safe_exit.py
@@ -50,7 +50,7 @@ class SafeExitTests(unittest.TestCase):
                 "action": "checkpoint",
             }
             with self.subTest(kwargs=kwargs), self.assertRaisesRegex(ExitError, message):
-                ExitEvent(**cast(dict[str, Any], {**args, **kwargs}))
+                cast(Any, ExitEvent)(**{**args, **kwargs})
         with self.assertRaisesRegex(ExitError, "identity"):
             SafeExitJournal("bad", 1)
         with self.assertRaisesRegex(ExitError, "identity"):
